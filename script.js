dd3ec63b1a51e9984253634480fd693ec2e6c24d
JeremiasVavak <jeremi.vavak@post.cz>
2022-08-02 11:04:17 +0200

    c

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("JavaScript funguje!");
+
+let vizualniHlavniOdstavec = document.querySelector(".hlavni-odstavec");
+vizualniHlavniOdstavec.textContent = "Domovská stránka"
+
+let vizualniMojeFoto = document.querySelector(".moje-foto");
+vizualniMojeFoto.src = "img/druhe-foto.jpeg"
+
+let vzdelani = document.querySelector(".vzdelani")
+vzdelani.textContent = "Školy"
+
+let predstaveniSe = document.querySelector(".predstaveni-se")
+predstaveniSe.style.color = "#0000FF"
+
+function pridejZvyrazneni() {
+    let vizualniTlacitko = document.querySelector("tlacitko1");
+    vizualniTlacitko.classList.add("zvyrazneny")
+}
